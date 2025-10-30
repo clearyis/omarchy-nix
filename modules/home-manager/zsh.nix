7d64143213e306d984556333a6e4997dd497c84f
Dylan <dylan@cleary.org>
2025-10-30 16:30:50 -0400

    fix noto fonts

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -2,8 +2,25 @@
 {
   programs.zsh = {
     enable = true;
+
+    envExtra = ''
+      setopt NO_GLOBAL_RCS
+    '';
+
     initContent = ''
       source "${pkgs.grml-zsh-config}/etc/zsh/zshrc"
+
+      # history
+      HISTSIZE=10000000
+
+      # nix-shell indicator
+      zstyle ':prompt:grml:right:setup' items
+      function nix_shell_prompt () {
+        REPLY=''${IN_NIX_SHELL+"(nix-shell) "}
+      }
+      grml_theme_add_token nix-shell-indicator -f nix_shell_prompt '%F{magenta}' '%F'
+      zstyle ':prompt:grml:left:setup' items rc nix-shell-indicator change-root user at host path vcs percent
     '';
   };
 }
+
