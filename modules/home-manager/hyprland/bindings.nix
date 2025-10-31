4106ca75160ad9eea9c967ceaa9870b933775afc
Dylan <dylan@cleary.org>
2025-10-31 15:55:22 -0400

    remove wofi keybind - use tofi instead

--- modules/home-manager/hyprland/bindings.nix
+++ modules/home-manager/hyprland/bindings.nix
@@ -9,7 +9,7 @@ in
 {
   wayland.windowManager.hyprland.settings = {
     bind = cfg.quick_app_bindings ++ [
-      "SUPER, SPACE, exec, wofi --show drun --sort-order=alphabetical"
+      # "SUPER, SPACE, exec, wofi --show drun --sort-order=alphabetical"
       "SUPER SHIFT, SPACE, exec, pkill -SIGUSR1 waybar"
       # "SUPER CTRL, SPACE, exec, ~/.local/share/omarchy/bin/swaybg-next"
       # "SUPER SHIFT CTRL, SPACE, exec, ~/.local/share/omarchy/bin/omarchy-theme-next"
