dca2a1bc9e0d4ac15b3c32930ba01fb444657ac0
Dylan <dylan@cleary.org>
2025-11-01 23:12:25 -0400

    change brightness key steps

--- modules/home-manager/hyprland/bindings.nix
+++ modules/home-manager/hyprland/bindings.nix
@@ -111,8 +111,8 @@ in
       ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
       ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
       ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-      ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
-      ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
+      ",XF86MonBrightnessUp, exec, brightnessctl set 655+"
+      ",XF86MonBrightnessDown, exec, brightnessctl set 655-"
     ];
 
     bindl = [
